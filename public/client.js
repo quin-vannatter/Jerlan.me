83f9ca6a9e9e712cc0bc2c427cb39153af40a3a4
Quinlan Vannatter <quin.vannatter@gmail.com>
2016-09-01 15:50:52 -0400

    Color Changes / Created Server
    Introduced the server. Added code for color changing. Currently set up
    to show how colors can change.

---
+++ public/client.js
@@ -0,0 +1,224 @@
+
+var TEST = {
+    r: false,
+    b: false,
+    g: false
+};
+
+(function() {
+
+    // Used in the drawing of the game.
+    var canvas;
+    var context;
+
+    // The background image of the game.
+    var imageBG;
+
+    // Objects specific to this client.
+    var player;
+    var camera;
+
+    // When the client loads, start initalization and game.
+    document.addEventListener("DOMContentLoaded",function() {
+
+        // Get the canvas.
+        canvas = document.getElementById('canvas');
+        context = canvas.getContext("2d");
+
+        // Create the background image.
+        imageBG = new Image();
+        imageBG.src = 'images/bg.png';
+
+        // Resize the canvas.
+        resizeCanvas();
+
+        // Start the game.
+        initalize();
+    });
+
+    /**
+     * Creates everything and starts the game loop.
+     */
+    function initalize() {
+        player = game.createObject("player");
+        camera = game.createObject("camera",{
+            focus: player,
+            size: {
+                x: canvas.width,
+                y: canvas.height
+            }
+        });
+
+        game.add(player);
+        game.add(camera);
+        gameLoop();
+    }
+
+    /**
+     * Updates the game, input and draws.
+     * @param {int} deltaTime The time difference between the last frame and the current frame.
+     */
+    function update(deltaTime) {
+        game.update(deltaTime);
+        draw();
+    }
+
+    /**
+     * Process of calculating time between frames and looping the game.
+     * @param {int} oldTime The time at the last frame of the game.
+     */
+    function gameLoop(oldTime) {
+        
+        // First frame assumes delta time is game interval.
+        if(typeof oldTime === 'undefined') {
+            oldTime = new Date() - game.GAME_INTERVAL;
+        }
+
+        // Get the current time.
+        time = new Date();
+
+        // Call the client's update function.
+        update(time - oldTime);
+
+        // Loop the game loop.
+        setTimeout(function () {
+            gameLoop(time);
+        },game.GAME_INTERVAL);
+    }
+
+    var a = false;
+
+    /**
+     * Draws all game objects in the game.
+     */
+    function draw() {
+
+        // Clear the canvas.
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // Create the background.
+        var backgroundPattern = context.createPattern(imageBG, 'repeat');
+        context.fillStyle = backgroundPattern;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Loop through the drawable game objects.
+        var objects = game.getDrawables();
+        for(var i = 0; i < objects.length; i++) {
+            var object = objects[i];
+
+            // If the object is visible;
+            if(object.visible) {
+
+                // Calculate canvas position based on camera.
+                var x = object.position.x - camera.position.x;
+                var y = object.position.y - camera.position.y;
+
+                // Draw the image.
+                context.drawImage(object.texture, x, y, object.size.x, object.size.y);
+                object.color.r += TEST.r ? 1 : -1;
+                object.color.b += TEST.b ? 1 : -1;
+                object.color.g += TEST.g ? 1 : -1;
+                if(object.color.g > 255 || object.color.g < 0) TEST.g = !TEST.g;
+                if(object.color.r > 255 || object.color.r < 0) TEST.r = !TEST.r;
+                if(object.color.b > 255 || object.color.b < 0) TEST.b = !TEST.b;
+                applyColor(object, x, y);
+            }
+        }
+    }
+
+    /**
+     * Applys the color property to the object.
+     * @param {object} object The drawable object that will have the color applied.
+     */
+    function applyColor(object, x, y) {
+        var color = object.color;
+
+        // Color being applied must be applied to an image with grey scale to work properly.
+
+        // Create a percentage from -100% to 100% to act as the increase to the current value of a pixel.
+        var percentage = {
+            r: (color.r / 256 * 2) - 1,
+            g: (color.g / 256 * 2) - 1,
+            b: (color.b / 256 * 2) - 1
+        }
+
+        // Get the image data of the image that was just drawn.
+        var imageData = context.getImageData(x, y, object.size.x, object.size.y);
+        var length = object.size.x * object.size.y * 4;
+
+        // Loop through each pixel.
+        for(var i = 0; i < length; i += 4) {
+
+            /* Apply the color to each pixel. The percentage is the lowest value working from either end of the pixel value.
+             * Eg. A value of 250 will have a 100% value of 5 because a value of 250 is too high of a change.
+             * So, in a case where red is 255, it will have an increase of 5 on a value of 5 or 250, where as a red value of 0 will
+             * have a change of -5 on 5 or 250.
+             */
+            imageData.data[i] = Math.min(imageData.data[i],256 - imageData.data[i]) * percentage.r + imageData.data[i];
+            imageData.data[i + 1] = Math.min(imageData.data[i + 1],256-imageData.data[i + 1]) * percentage.g + imageData.data[i + 1];
+            imageData.data[i + 2] = Math.min(imageData.data[i + 2],256-imageData.data[i + 2]) * percentage.b + imageData.data[i + 2];
+
+            // If any pixel values go past 0 or 255, apply the difference to the other two pixel values.
+            // Eg. a red pixel of 265 would become 255 while blue and green would increase by 10.
+            if(imageData[i] > 255) {
+                var diff = imageData[i] - 256;
+                imageData.data[i + 1] += diff;
+                imageData.data[i + 2] += diff;
+                imageData[i] = 0;
+            } else if(imageData[i] < 0) {
+                imageData.data[i + 1] += imageData[i];
+                imageData.data[i + 2] += imageData[i];
+                imageData[i] = 0;
+            }
+
+            if(imageData[i + 1] > 255) {
+                var diff = imageData[i + 1] - 256;
+                imageData.data[i] += diff;
+                imageData.data[i + 2] += diff;
+                imageData[i] = 0;
+            } else if(imageData[i + 1] < 0) {
+                imageData.data[i] += imageData[i + 1];
+                imageData.data[i + 2] += imageData[i + 1];
+                imageData[i + 1] = 0;
+            }
+
+            if(imageData[i + 2] > 255) {
+                var diff = imageData[i + 2] - 256;
+                imageData.data[i] += diff;
+                imageData.data[i + 1] += diff;
+                imageData[i + 2] = 0;
+            } else if(imageData[i + 2] < 0) {
+                imageData.data[i] += imageData[i + 2];
+                imageData.data[i + 1] += imageData[i + 2];
+                imageData[i + 2] = 0;
+            }
+        }
+
+        // Apply the changes to the context.
+        context.putImageData(imageData, x, y);
+    }
+
+    /**
+     * Resizes the canvas to fit the window.
+     * Also updates the camera to match the change.
+     */
+    function resizeCanvas() {
+
+        // Set the canvas width and height.
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        // Update the camera if it is set.
+        if(typeof camera !== 'undefined') {
+
+            // Set the camera size.
+            camera.size = {
+                x: canvas.width,
+                y: canvas.height
+            };
+
+            // Update the center location of the camera.
+            camera.updateSize();
+        }
+    }
+})();
---
+++ public/game.js
@@ -0,0 +1,326 @@
+var game = {};
+
+/**
+ * Anonymous function that runs the game script.
+ */
+(function() {
+
+    // Game properties.
+    var GAME_INTERVAL = 1000 / 60;
+
+    var DEFAULT = {
+        default: {
+            position: {x: 0, y: 0},
+            velocity: {x: 0, y: 0},
+            size: {x: 0, y: 0},
+            enabled: true,
+            drawable: true,
+            visible: true,
+            collidable: true,
+            destroy: false,
+            color: {r: 0, b:0, g:0}
+        },
+        player: {
+            type: 'player',
+            texture: 'images/player.png',
+            id: -1,
+            speed: 5,
+            size: {x: 50, y:50},
+            color: {r: 110, b:200, g:34},
+            update: updatePlayer
+        },
+        wall: {
+            type: 'wall',
+            texture: 'images/wall.png',
+            unitSize: {x: 50, y: 50},
+            size: {x: 1, y: 1},
+        },
+        shot: {
+            type: 'shot',
+            texture: 'images/shot.png',
+            size: {x: 13, y: 13},
+            update: updateShot
+        },
+        camera: {
+            type: 'camera',
+            zoom: {x: 1, y: 1},
+            focus: null,
+            drawable: false,
+            ease: 5,
+            center: {x: 0, y: 0},
+            initalize: updateSize,
+            updateSize: updateSize,
+            update: updateCamera
+        }
+    }
+
+    // Objects that update and draw.
+    var objects = {};
+
+    // Objects that can be created.
+
+    /**
+     * Creates an object.
+     * 
+     * @param {string} type The type of the object.
+     * @param {object} data The data of the object.
+     */
+    function createObject(type, data) {
+
+        // Create the default object.
+        var object = copy(DEFAULT.default);
+
+        // Add default properties for that type.
+        for(var prop in DEFAULT[type]) {
+            if(DEFAULT[type].hasOwnProperty(prop)) {
+
+                // Add value if not object, copy if it is.
+                if(typeof DEFAULT[type][prop] !== 'object') {
+                    object[prop] = DEFAULT[type][prop];
+                } else {
+                    object[prop] = copy(DEFAULT[type][prop]);
+                }
+            }
+        }
+
+        // Loop through and set assigned properties of the player.
+        for(var prop in data) {
+            if(data.hasOwnProperty(prop)) {
+                object[prop] = data[prop];
+            }
+        }
+
+        // Run object initalization code if there is any.
+        if(typeof object.initalize !== 'undefined') {
+            object.initalize();
+        }
+
+        // Set the texture as an image, if there is a texture.
+        if(typeof object.texture !== 'undefined') {
+            var image = new Image();
+            image.src = object.texture;
+            object.texture = image;
+        }
+
+        return object;
+    }
+
+    /**
+     * Update function for a player.
+     * @param {int} deltaTime The amount of time since last update.
+     */
+    function updatePlayer(deltaTime) {
+        move(this, deltaTime);
+    }
+
+    /**
+     * Update function for a shot.
+     * @param {int} deltaTime The amount of time since last update.
+     */
+    function updateShot(deltaTime) {
+        move(this, deltaTime);
+    }
+
+    /**
+     * Update function for a camera.
+     * @param {int} deltaTime The amount of time since last update.
+     */
+    function updateCamera(deltaTime) {
+
+        // If there is a focus object, move the camera towards it.
+        if(this.focus != null) {
+            this.velocity = {x: 0, y: 0};
+            
+            this.position.x += ((this.focus.position.x + this.center.x) - this.position.x) / 
+                this.ease * deltaTime / GAME_INTERVAL;
+
+            this.position.y += ((this.focus.position.y + this.center.y) - this.position.y) / 
+                this.ease * deltaTime / GAME_INTERVAL;
+        }
+
+        // Move the camera by the velocitiy.
+        move(this, deltaTime);
+    }
+
+    /**
+     * Update function for a camera frame.
+     */
+    function updateSize() {
+
+        // If there is a focus object, update the center value.
+        if(this.focus != null) {
+            this.center = {
+                x: this.focus.size.x/2 - this.size.x/2,
+                y: this.focus.size.y/2 - this.size.y/2
+            }
+        }
+    }
+
+    /**
+     * Copys an object and returns it.
+     * @param {object} object The object being copied.
+     * @return The copy of the object.
+     */
+    function copy(object) {
+
+        // Object that will be returned.
+        var result = {};
+
+        // Loop through and add each property to the object.
+        for(var prop in object) {
+            if(object.hasOwnProperty(prop)) {
+
+                // If the value isn't an object.
+                if(typeof object[prop] !== 'object') {
+                    result[prop] = object[prop];
+                } else {
+                    result[prop] = copy(object[prop]);
+                }
+            }
+        }
+
+        // Return the result.
+        return result;
+    }
+
+     /**
+     * Creates a wall which interacts with other game objects.
+     * 
+     * @param {object} position The position of the wall.
+     * @param {object} size The size of the wall. Expected in units of walls.
+     * @return a new wall object.
+     */
+    function wall(position, size) {
+
+        // Wall texture.
+        var texture = new Image();
+        texture.src = WALL_TEXTURE;
+
+        // Wall size
+        var wallSize = {
+            x: size.x * WALL_UNIT_SIZE.x,
+            y: size.y * WALL_UNIT_SIZE.y
+        };
+        
+        // Create the player and return it.
+        return {
+            type: WALL_TYPE,
+            position: position,
+            size: wallSize
+        }
+    }
+
+    // Functions that game objects can use.
+
+    /**
+     * Given that the passed in object has a velocitiy and position,
+     * this function will move the player by its velocitiy.
+     * @param {object} object The object being moved.
+     * @param {int} deltaTime The amount of time since last update.
+     */
+    function move(object, deltaTime) {
+        object.position.x += (object.velocity.x * deltaTime / GAME_INTERVAL);
+        object.position.y += (object.velocity.y * deltaTime / GAME_INTERVAL);
+    }
+
+    /**
+     * Calculates the distance between two points.
+     * @param {object} posA Position A value.
+     * @param {object} posB Position B value.
+     */
+    function getDistance(posA, posB) {
+        return Math.sqrt(Math.pow(posB.x - posA.x, 2) + Math.pow(posB.y - posA.y, 2));
+    }
+
+    // Functions that run the game.
+
+    /**
+     * Loops through the objects and calls the update function.
+     * @param {int} deltaTime The amount of time since last update.
+     */
+    function update(deltaTime) {
+
+        // Loop through all the game objects.
+        for(var prop in objects) {
+            if(objects.hasOwnProperty(prop)) {
+                var type = objects[prop];
+
+                // Loop through each object for the current type.
+                for(var i = type.length - 1; i >= 0; i--) {
+                    var object = type[i];
+
+                    // Remove if destroy is true.
+                    if(object.destroy) {
+                        type.splice(i,1);
+                        continue;
+                    }
+
+                    // Update if there is an update function.
+                    if(typeof object.update !== 'undefined') {
+                        object.update(deltaTime);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds an object to the game objects list.
+     * @param {object} object The object being added.
+     */ 
+    function add(object) {
+
+        // Create if not created.
+        if(typeof objects[object.type] === 'undefined') {
+            objects[object.type] = [];
+        }
+
+        // Add the object.
+        objects[object.type].push(object);
+    }
+
+    /**
+     * Creates the map for the current game.
+     */ 
+    function createMap(map) {
+        var walls = map[0];
+        var spawns = map[1];
+
+    }
+
+    /**
+     * Gets all objects that can be drawn.
+     * @return All the game objects that can be drawn.
+     */
+    function getDrawables() {
+
+        var drawables = [];
+
+         // Loop through all the game objects.
+        for(var prop in objects) {
+            if(objects.hasOwnProperty(prop)) {
+                var type = objects[prop];
+
+                // Loop through each object for the current type.
+                for(var i = type.length - 1; i >= 0; i--) {
+                    var object = type[i];
+
+                    // Add to drawables if drawable.
+                    if(object.drawable) {
+                        drawables.push(object);
+                    }
+                }
+            }
+        }
+
+        return drawables;
+    }
+    
+    // Creating the public connections from server/client to game.
+    game.GAME_INTERVAL = GAME_INTERVAL;
+    game.getDrawables = getDrawables;
+    game.update = update;
+    game.createMap = createMap;
+    game.createObject = createObject;
+    game.add = add;
+})();
